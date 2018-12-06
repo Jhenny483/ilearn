efd24baa730c510d08f288d7d451617468da1713
unknown <jhennysilva483@gmail.com>
2018-12-06 03:55:20 -0200

    ajustes css

--- ilearn-novo/notificacao.php
+++ ilearn-novo/notificacao.php
@@ -2,33 +2,61 @@
 include "superior.php";
 ?>
 
-        <!-- sidebar-wrapper  -->
-        <main class="page-content">
-            <div class="container-fluid margin-superior-menu">
-               <div class="col-md-12">
-               <div class="titulo"><h1>Notificações</h1></div>
-               <div class="row">
-                   <div class="caixaNotificacao">
-                       <div class="linhaNotificacao">
-                           <p><i class="fa fa-power-off"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                           <p><i class="fa fa-power-off"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                           <p><i class="fa fa-power-off"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                           <p><i class="fa fa-power-off"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                           <p><i class="fa fa-power-off"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                           <p><i class="fa fa-power-off"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                           <p><i class="fa fa-power-off"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                           <p><i class="fa fa-power-off"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                       </div>
-                   </div>
-               </div>
-               
-               
-                </div></div>
-        </main>
-        <!-- page-content" -->
-   
+    <!-- sidebar-wrapper  -->
+    <main class="page-content">
+        <div class="container-fluid margin-superior-menu">
+            <div class="col-md-12">
+                <div class="titulo">
+                    <h1>Notificações</h1>
+                </div>
+                <div class="row">
+                    <div class="caixaNotificacao">
+                        <div class="linhaNotificacao">
+                            <table class="table table-hover table-dark">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nome</th>
+                                        <th scope="col">Notificação</th>
+                                        <th scope="col">Data</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Nome do usuario</td>
+                                        <td>Texto notificação</td>
+                                        <td>66/66/6666</td>
+                                    </tr>
+
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Nome do usuario</td>
+                                        <td>Texto notificação</td>
+                                        <td>66/66/6666</td>
+                                    </tr>
+
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Nome do usuario</td>
+                                        <td>Texto notificação</td>
+                                        <td>66/66/6666</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
 
 
-<?php
+            </div>
+        </div>
+    </main>
+    <!-- page-content" -->
+
+
+
+    <?php
 include "inferior.php";
 ?>
